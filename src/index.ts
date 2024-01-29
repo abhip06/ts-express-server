e22a475b010daafaf9086224d5e637c7c207231d
Abhinav Patel <abhipatel1806@gmail.com>
2024-01-29 12:59:57 +0530

    test controllers added

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import express, {Express, Request, Response} from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process?.env?.PORT || 8080;
+const PORT = process.env?.PORT || 8080;
 
 const app: Express = express();
+
+// middleware declaration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
 
 // routes imports
 import testRoutes from "./routes/test.route.js";
 
+// routes declaration
 app.use("/api/v1/test", testRoutes);
 
 app.listen(PORT, () => {
